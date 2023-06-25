af96c11de907d64593ec8119d11337fa5aaf940f
Gabriel Silva <102487234+GabrielCarvalhouo@users.noreply.github.com>
2023-06-24 22:35:33 -0300

    Update insert component button in Detalhes.js
    Add component Botao

--- src/telas/Cesta/componentes/Detalhes.js
+++ src/telas/Cesta/componentes/Detalhes.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Image, StyleSheet, View, TouchableOpacity } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
 import Texto from '../../../componentes/Texto';
+import Botao from '../../../componentes/Botao';
 
 export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco, botao }) {
   return <>
@@ -12,10 +13,7 @@ export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, pr
     </View>
     <Texto style={estilos.descricao}>{ descricao }</Texto>
     <Texto style={estilos.preco}>{ preco }</Texto>
-
-    <TouchableOpacity style={estilos.botao} onPress={() => {}}>
-      <Texto style={estilos.textoBotao}>{ botao }</Texto>
-    </TouchableOpacity>
+    <Botao>{ botao }</Botao>
   </>
 }
 
@@ -50,18 +48,5 @@ const estilos = StyleSheet.create({
     fontSize: 26,
     lineHeight: 42,
     marginTop: 8,
-  },
-  botao: {
-    marginTop: 16,
-    backgroundColor: "#2A9F85",
-    paddingVertical: 16,
-    borderRadius: 6,
-  },
-  textoBotao: {
-    textAlign: "center",
-    color: "#ffffff",
-    fontSize: 16,
-    lineHeight: 26,
-    fontWeight: "bold",
   },
 })
